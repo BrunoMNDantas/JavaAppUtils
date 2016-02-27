a5fcacc7db8d0d36bad73dd3e15edf275d71094a
BrunoMNDantas <bmndantas@gmail.com>
2016-02-27 19:11:21 +0000

    Log

---
+++ log/log/src/main/java/app/utils/log/StringUtils.java
@@ -0,0 +1,13 @@
+package app.utils.log;
+
+public class StringUtils {
+
+	public static String padRight(String s, int n) {
+	     return String.format("%1$-" + n + "s", s);  
+	}
+
+	public static String padLeft(String s, int n) {
+	    return String.format("%1$" + n + "s", s);  
+	}
+
+}
